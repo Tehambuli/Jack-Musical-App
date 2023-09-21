fc176405fa2ebc02df175a804af1961866ec4261
Erin E.  Smith <TehambuliNire@Hotmail.com>
2023-09-20 21:30:01 -0400

    Update app.js
    Added Alphabet, enter, and backspace key

--- app.js
+++ app.js
@@ -1,2 +1,35 @@
 const tileDisplay = document.querySelector('.tile-container')
 const keyboard = document.querySelector('.key-container')
+
+
+const keys = [
+
+    'Q',
+    'W',
+    'E',
+    'R',
+    'T',
+    'Y',
+    'U',
+    'I',
+    'O',
+    'P',
+    'A',
+    'S',
+    'D',
+    'F',
+    'G',
+    'H',
+    'J',
+    'K',
+    'L',
+    'ENTER',
+    'Z',
+    'X',
+    'C',
+    'V',
+    'B',
+    'H',
+    '<',
+
+]
