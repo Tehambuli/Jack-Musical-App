b7be46a2c282602d1996037aae4072d4d4927aac
Erin E.  Smith <TehambuliNire@Hotmail.com>
2023-10-15 18:47:40 -0400

    Revised Keyboard
    Revised Keyboard, now it looks better on the APP. Beautified the JS code as well!

---
+++ app.js
@@ -0,0 +1,42 @@
+const tileDisplay = document.querySelector('.tile-container')
+const keyboard = document.querySelector('.key-container')
+
+
+const keys = [
+    'Q',
+    'W',
+    'E',
+    'R',
+    'T',
+    'Y',
+    'U',
+    'I',
+    'O',
+    'P',
+    'A',
+    'S',
+    'D',
+    'F',
+    'G',
+    'H',
+    'J',
+    'K',
+    'L',
+    'ENTER',
+    'Z',
+    'X',
+    'C',
+    'V',
+    'B',
+    'N',
+    'M',
+    '«',
+]
+
+
+keys.forEach(key => {
+   const buttonElement = document.createElement('button')
+   buttonElement.textContent = key
+   keyboard.append(buttonElement)
+
+})
