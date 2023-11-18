8b53aef96ba898469805f8ee8818243bdaed656e
Erin E.  Smith <TehambuliNire@Hotmail.com>
2023-11-17 20:25:48 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -2,16 +2,16 @@
 const songLookup = {
     // Add the rest of the songs in the same format
 
-   'DENVER': 'https://www.youtube.com/watch?v=vq4hRDnGbDY',
-   'DRIP': ' https://www.youtube.com/watch?v=4-SJyuCFD18',
-   'GHOST': 'https://www.youtube.com/watch?v=GByTR0pBYWE',
+   'DENVER':  'https://www.youtube.com/watch?v=vq4hRDnGbDY',
+   'DRIP':    'https://www.youtube.com/watch?v=4-SJyuCFD18',
+   'GHOST':   'https://www.youtube.com/watch?v=GByTR0pBYWE',
    'SUNDOWN': 'https://www.youtube.com/watch?v=N2-dqe8qweY',  
    'FRIENDS': 'https://www.youtube.com/watch?v=kM-4va2nuYg',
-   'HEAVY': 'https://www.youtube.com/watch?v=BeFbMwLSszI', 
-   'NIGHT': ' https://www.youtube.com/watch?v=wPrEkA_gQp4', 
-   'POPPIN':   'https://www.youtube.com/watch?v=w9uWPBDHEKE', 
+   'HEAVY':   'https://www.youtube.com/watch?v=BeFbMwLSszI', 
+   'NIGHT':   'https://www.youtube.com/watch?v=wPrEkA_gQp4', 
+   'POPPIN':  'https://www.youtube.com/watch?v=w9uWPBDHEKE', 
    'TYLER':   'https://www.youtube.com/watch?v=np9Ub1LilKU',
-   'LOVIN': 'https://www.youtube.com/watch?v=Iq8h3GEe22o',
+   'LOVIN':   'https://www.youtube.com/watch?v=Iq8h3GEe22o',
 
 };
 
